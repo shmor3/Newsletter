a97d4adc6bd81ffbf64196ed6e61d4bb737a272c
NParel <74482909+nparel@users.noreply.github.com>
2021-02-25 04:09:03 -0500

    h

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import Layout from "@utils/layout"
 import SEO from "@utils/seo"
-import Img from "gatsby-image"
-import Button from "@atoms/button"
-import ContentBlock from "@atoms/contentBlock"
 import Container from "@atoms/container"
-import List from "@atoms/list"
-import Subheading from "@atoms/subheading"
 import SectionCheckout from "@molecules/sectionCheckout"
 
 const IndexPage = () => {
@@ -41,38 +36,6 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <ContentContainer content grid gridSplit>
-        <Image>
-          <Img fluid={data.moonclerk.childImageSharp.fluid} />
-        </Image>
-        <ContentBlock>
-          <Subheading>MoonClerk</Subheading>
-          <h2>
-            Start accepting recurring and one-time online payments in 5 minutes.
-          </h2>
-          <p>
-            No coding, merchant account, or hosting required. Create completely
-            customizable forms that can be embedded or shared anywhere. Automate
-            and integrate your processes with built-in tools and 3rd party
-            integrations.
-          </p>
-          <List>
-            <li>Send funds to your bank account automatically</li>
-            <li>Integrate with hundreds of third party providers</li>
-            <li>Take granular control of your payment process</li>
-            <li>Style the entire checkout process to match your brand</li>
-            <li>
-              Power up with extensions: Digital Delivery, Mailchimp Pro, etc...
-            </li>
-          </List>
-          <Button
-            href="https://moonclerk.com"
-            aria-label="Learn more about MoonClerk"
-          >
-            Learn More
-          </Button>
-        </ContentBlock>
-      </ContentContainer>
       <SectionCheckout />
     </Layout>
   )
@@ -80,14 +43,3 @@ const IndexPage = () => {
 
 export default IndexPage
 
-const Image = styled.div``
-
-const ContentContainer = styled(Container)`
-  &:nth-of-type(odd) {
-    direction: rtl;
-
-    div {
-      direction: ltr;
-    }
-  }
-`
