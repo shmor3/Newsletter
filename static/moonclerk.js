17db6e9ee33a9fc856ebc3fbcff26b9d4a5aa6e3
NParel <74482909+nparel@users.noreply.github.com>
2021-02-25 03:36:04 -0500

    fixes

--- static/moonclerk.js
+++ static/moonclerk.js
@@ -14,4 +14,3 @@ var mc5exp0qlk2y40
   var scr = d.getElementsByTagName(t)[0]
   scr.parentNode.insertBefore(s, scr)
 })(document, "script")
-<div id="mc2dobl7zmzo2p"><a href="https://app.moonclerk.com/pay/2dobl7zmzo2p">Subscribe</a></div><script type="text/javascript">var mc2dobl7zmzo2p;(function(d,t) {var s=d.createElement(t),opts={"checkoutToken":"2dobl7zmzo2p","width":"100%"};s.src='https://d2l7e0y6ygya2s.cloudfront.net/assets/embed.js';s.onload=s.onreadystatechange = function() {var rs=this.readyState;if(rs) if(rs!='complete') if(rs!='loaded') return;try {mc2dobl7zmzo2p=new MoonclerkEmbed(opts);mc2dobl7zmzo2p.display();} catch(e){}};var scr=d.getElementsByTagName(t)[0];scr.parentNode.insertBefore(s,scr);})(document,'script');</script>
