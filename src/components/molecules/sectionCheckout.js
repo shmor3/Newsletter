36cf1095b6604d9aeb50d48fc7d05d2ab43da948
NParel <74482909+nparel@users.noreply.github.com>
2021-02-25 03:52:57 -0500

    fix? x4

--- src/components/molecules/sectionCheckout.js
+++ src/components/molecules/sectionCheckout.js
@@ -32,7 +32,7 @@ const SectionCheckout = () => {
           <Seperator />
           <SliderTestimonials />
         </Content>
-        {<div id="mc2dobl7zmzo2p"><a href="https://app.moonclerk.com/pay/2dobl7zmzo2p">Subscribe</a></div><script type="text/javascript">var mc2dobl7zmzo2p;(function(d,t) {var s=d.createElement(t),opts={"checkoutToken":"2dobl7zmzo2p","width":"100%"};s.src='https://d2l7e0y6ygya2s.cloudfront.net/assets/embed.js';s.onload=s.onreadystatechange = function() {var rs=this.readyState;if(rs) if(rs!='complete') if(rs!='loaded') return;try {mc2dobl7zmzo2p=new MoonclerkEmbed(opts);mc2dobl7zmzo2p.display();} catch(e){}};var scr=d.getElementsByTagName(t)[0];scr.parentNode.insertBefore(s,scr);})(document,'script');</script>}
+        {/* PASTE MOONCLERK FORM HTML ID HERE */}
         <Form id="mc5exp0qlk2y40" ref={scriptEl} />
       </CheckoutContainer>
     </Container>
