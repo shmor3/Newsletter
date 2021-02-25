1fd41b9ee940aa393472b1a668eb0be516eb2de1
NParel <74482909+nparel@users.noreply.github.com>
2021-02-25 03:31:32 -0500

    homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,6 @@ import ContentBlock from "@atoms/contentBlock"
 import Container from "@atoms/container"
 import List from "@atoms/list"
 import Subheading from "@atoms/subheading"
-import SectionHeader from "@molecules/sectionHeader"
-import SectionSetup from "@molecules/sectionSetup"
 import SectionCheckout from "@molecules/sectionCheckout"
 
 const IndexPage = () => {
@@ -43,8 +41,6 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <SectionHeader />
-      <SectionSetup />
       <ContentContainer content grid gridSplit>
         <Image>
           <Img fluid={data.moonclerk.childImageSharp.fluid} />
@@ -72,65 +68,6 @@ const IndexPage = () => {
           <Button
             href="https://moonclerk.com"
             aria-label="Learn more about MoonClerk"
-          >
-            Learn More
-          </Button>
-        </ContentBlock>
-      </ContentContainer>
-      <ContentContainer content grid gridSplit>
-        <Image>
-          <Img fluid={data.gatsbyjs.childImageSharp.fluid} />
-        </Image>
-        <ContentBlock>
-          <Subheading>Gatsby</Subheading>
-          <h2>
-            Create blazing fast apps and websites without needing to become a
-            performance expert.
-          </h2>
-          <p>
-            Gatsby is an open source, modern website framework that builds
-            performance into every site by leveraging the latest web
-            technologies such as React and GraphQL.
-          </p>
-          <List>
-            <li>Modern web tech without the headache</li>
-            <li>Bring your own data</li>
-            <li>Scale to the entire internet</li>
-            <li>Future-proof your website</li>
-            <li>Progressive Web Apps</li>
-          </List>
-          <Button
-            href="https://gatsby.org"
-            aria-label="Learn more about Gatsby Js"
-          >
-            Learn More
-          </Button>
-        </ContentBlock>
-      </ContentContainer>
-      <ContentContainer content grid gridSplit>
-        <Image>
-          <Img fluid={data.netlify.childImageSharp.fluid} />
-        </Image>
-        <ContentBlock>
-          <Subheading>Netlify</Subheading>
-          <h2>
-            Run lightning-fast websites globally, with every change deployed
-            automatically.
-          </h2>
-          <p>
-            The fastest way to build the fastest sites. Build, test, and deploy
-            globally with Netlify’s all-in-one platform for modern web projects
-          </p>
-          <List>
-            <li>Modern web tech without the headache</li>
-            <li>Bring your own data</li>
-            <li>Scale to the entire internet></li>
-            <li>Future-proof your website</li>
-            <li>Progressive Web Apps</li>
-          </List>
-          <Button
-            href="https://netlify.com"
-            aria-label="Learn more about Netlify"
           >
             Learn More
           </Button>
